2159c781d302fbc56b2bdc7d8160994ba905f2ba
Sirisha Ayyagari <ayyagari.siri@gmail.com>
2019-11-05 06:20:38 +0100

    Add Rect and Sqr

---
+++ src/be/intecbrussel/sample/Rect.java
@@ -0,0 +1,12 @@
+package be.intecbrussel.sample;
+
+public class Rect {
+    
+    private int l;
+    private int w;
+
+
+
+
+    public final String DESC = "RECT";
+}
---
+++ src/be/intecbrussel/sample/Squr.java
@@ -0,0 +1,17 @@
+package be.intecbrussel.sample;
+
+public class Squr extends  Rect{
+    public final String DESC = "SQUR";
+    public static void main(String[] args) {
+        Squr sq = new Squr();
+
+
+
+
+
+        System.out.println(sq.DESC);
+
+        Rect rec = new Squr();
+        System.out.println(rec.DESC);
+    }
+}
