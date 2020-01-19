7775efbaec3a2952d5055f59846c8eb6d7159eee
Sirisha Ayyagari <ayyagari.siri@gmail.com>
2020-01-19 23:30:45 +0100

    FundamentalsTestPractice

---
+++ src/be/intecbrussel/collections/CollectionMain.java
@@ -0,0 +1,65 @@
+package be.intecbrussel.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class CollectionMain {
+
+    public static void main(String[] args) {
+
+        Collection<String> collection = new ArrayList<>();
+        collection.add("Kalyan");
+        collection.add("Sirisha");
+        collection.add("Satwika");
+
+
+        System.out.println(collection.size());
+
+        List<String> list = new ArrayList<>();
+        list.add("Kalyan");
+        list.add("Sirisha");
+        list.add("Satwika");
+
+        System.out.println(list.size());
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Kalyan");
+        arrayList.add("Sirisha");
+        arrayList.add("Satwika");
+
+        String[] array = list.toArray(new String[0]);
+        String[] anotherArray = list.toArray(String[]::new);
+
+        String[] names = {"Kalyan", "Sirisha", "Satwika"};
+
+        List<String> namesList = Arrays.asList(names);
+
+
+        System.out.println(arrayList.size());
+
+        list.add("name");
+        list.remove("name");
+        list.remove(1);
+
+        boolean exists = list.contains("name");
+
+        list.indexOf("name");
+// list.isEmpty() is same as list.size() == 0
+
+        list.get(0);
+
+        list.add("something");
+        list.add("anmoher");
+        list.add("eekls");
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i)); // array is array[i], but list is list.get(i)
+
+        }
+
+        list.forEach(System.out::print);
+
+    }
+}
---
+++ src/be/intecbrussel/collections/Exercise1.java
@@ -0,0 +1,53 @@
+package be.intecbrussel.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Integer> inputList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Please enter number");
+            int num = scan.nextInt();
+            inputList.add(num);
+        }
+
+        inputList.forEach(System.out::println);
+        System.out.println(inputList.stream().mapToDouble(a -> a).sum());
+        System.out.println(inputList.stream().mapToDouble(a -> a).average().getAsDouble());
+
+        int sum = 0;
+        for (int i = 0; i < inputList.size(); i++) {
+            sum = sum + inputList.get(i);
+        }
+
+        System.out.println("Sum : " + sum);
+        System.out.println("avg : " + sum * 1d / inputList.size() * 1d);
+
+        String sentence = null;
+        List<String> sentenceList = new ArrayList<>();
+        do {
+            System.out.println("Please enter a sentence ending with .");
+            sentence = scan.next();
+            if (sentence.endsWith(".")) {
+                break;
+            } else {
+                System.out.println("adding word  : " + sentence);
+                sentenceList.add(sentence);
+            }
+        } while (true);
+
+        sentenceList.forEach(System.out::println);
+        List<String> reverseList = new ArrayList<>();
+        for (int i = sentenceList.size() -1; i >= 0; i--) {
+            reverseList.add(sentenceList.get(i));
+        }
+
+        reverseList.forEach(System.out::println);
+    }
+
+}
